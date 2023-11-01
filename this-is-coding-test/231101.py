1d2585e47c968337490ac6f44b430053b0d42bf3
seoyeonaa <skyofaria3856@gmail.com>
2023-11-01 18:58:03 +0900

    Greedy(거스름돈)

--- this-is-coding-test/231101.py
+++ this-is-coding-test/231101.py
@@ -1,5 +1,5 @@
 # page 86; Greedy Algorithm
-# 거스름돈 1260원을 동전으로 줄 시 최소의 동전 개수는?
+# 거스름돈 1260원을 동전으로 줄 시 거슬러 주는 횟수는?
 changes = 1260
 coins = [500, 100, 50, 10]
 
@@ -7,14 +7,14 @@ coins = [500, 100, 50, 10]
 # 나의 풀이
 def givechanges(changes, coins):
     print(f"거슬러 줘야 할 돈은 {n}원 입니다.")
-    count = 0
-    while changes > 0:
-        if changes // max(coins) >= 1:
+    count = 0  # 횟수 0부터 시작
+    while changes > 0:  # 거스름돈이 0보다 클 때 까지
+        if changes // max(coins) >= 1:  # 한 번이라도 거슬러 줄 수 있는지?
             print(f"{max(coins)}을 거슬러 줍니다.")
             changes -= max(coins)
             count += 1
 
-        else:
+        else:  # 해당 단위의 동전으로 못 거슬러 준다면?
             coins.remove(max(coins))
 
     return f"총 {count}번 거슬러 주었습니다."
